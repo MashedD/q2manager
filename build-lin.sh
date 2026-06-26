bb3c2704b647a6a05c70c98591a7afa4b11731ca
MashedD <sl8code@gmail.com>
2026-06-26 12:32:22 +0200

    Update build script

--- build-lin.sh
+++ build-lin.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env -S bash -Eeuo pipefail
 cd "$(dirname -- "$(readlink -f -- "$0")")"
 
+mkdir -p dist
 cmake -S . -B build
-cmake --build build --config Release 
+cmake --build build --config Release
 sstrip build/q2manager
+cp -f build/q2manager dist/q2manager
 
